6dbd5e615c4677735024bb4ca57bd54ea74b986a
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-09-28 22:13:05 -0300

    Just a bit better

--- cdh_server.rb
+++ cdh_server.rb
@@ -4,19 +4,20 @@ require 'digest/md5'
 
 LI_DIRECTION_OUTPUT = 0x20
 LI_CMD_RECEIVE_DATA = 0x04
+TELECOMMAND_KEY = "32_BYTESFORAVERYSECURESECRETKEY" + 0.chr
+CALLSIGN = "012345"
+PORT = 10000
+AX25_HEADER = CALLSIGN + (0x01).chr + CALLSIGN + [0x01,0x03,0xF0].map(&:chr).join
 
 sem = Mutex.new
-
 received_data = []
-
-telecommand_key = "32_BYTESFORAVERYSECURESECRETKEY" + 0.chr
-callsign = "012345"
 
 def compute_mac(data,seq_nr,key)
 	digest = Digest::MD5.new
 	digest << key
 	
-	digest << (seq_nr.to_i & 0xFF).chr + ((seq_nr.to_i >> 8) & 0xFF).chr + ((seq_nr.to_i >> 16) & 0xFF).chr + ((seq_nr.to_i >> 24) & 0xFF).chr
+	digest << (seq_nr.to_i & 0xFF).chr + ((seq_nr.to_i >> 8) & 0xFF).chr +
+		((seq_nr.to_i >> 16) & 0xFF).chr + ((seq_nr.to_i >> 24) & 0xFF).chr
 	
 	digest << data
 	
@@ -36,7 +37,31 @@ def fletcher_checksum16(data,count,accum)
 	sum1 << 8 | sum2
 end
 
-@server = TCPServer.new 10000
+def build_packet(seq_nr, command)
+	
+	seq_buf =((seq_nr.to_i >> 16) & 0xFF).chr + ((seq_nr.to_i >> 8) & 0xFF).chr +   (seq_nr.to_i & 0xFF).chr
+
+	app_payload = compute_mac(command,seq_nr,TELECOMMAND_KEY) + seq_buf + command
+	#p "Payload:"
+	#p app_payload.split('').map(&:ord).map{|e| e.to_s(16)}
+
+	ax25_packet = AX25_HEADER + app_payload + "aaaa" # aaaa is a fake CRC
+	#p "AX25:"
+	#p ax25_packet.split('').map(&:ord).map{|e| e.to_s(16)}
+	p_size = ax25_packet.length
+	li_header = [LI_DIRECTION_OUTPUT,LI_CMD_RECEIVE_DATA,p_size >> 8, p_size & 0xFF].map(&:chr).join
+	chksum1 = fletcher_checksum16(li_header,li_header.length,0)
+	chksum1_str = [chksum1 >> 8, chksum1 & 0xFF].map(&:chr).join
+	
+	chksum2 = fletcher_checksum16(chksum1_str,2,chksum1)
+	chksum2 = fletcher_checksum16(ax25_packet,p_size,chksum2)
+	
+	chksum2_str = [chksum2 >> 8, chksum2 & 0xFF].map(&:chr).join
+	
+	"He" + li_header + chksum1_str + ax25_packet + chksum2_str
+end
+
+@server = TCPServer.new PORT
 
 def wait_for_manolito
 	STDOUT.puts 'Manolito is not online.'
@@ -60,9 +85,10 @@ t = Thread.new do
 			end
 		}
 		if !received_data.empty?
+			s.print "\r\n"
 			s.puts "manolito (ascii)>" + received_data.map(&:to_s).join
 			s.puts "manolito (hex)  > " + received_data.map{|c| c.ord.to_s(16)}.join(' ')
-			s.puts "ground control  > "
+			s.print "ground control  > "
 			received_data = []
 		end
 		sleep 0.001
@@ -72,57 +98,22 @@ end
 seq_nr = 0
 while(1) do
 	
-	puts 'ground control  > '
-	message = gets
+	print 'ground control  > '
+	message = gets.strip
+	
+	
+	if message == 'exit'
+		exit
+	end
+	
 	puts 'Sending'
 
 	seq_nr += 1
-	seq_buf =((seq_nr.to_i >> 16) & 0xFF).chr + ((seq_nr.to_i >> 8) & 0xFF).chr +   (seq_nr.to_i & 0xFF).chr
+	command = message.split(' ').map{|c| c.to_i(16)}.map(&:chr).join
+	packet = build_packet(seq_nr, command)
 	
-	
-	command = message.split(' ').map{|c| c.to_i(16)}.map(&:chr).join # SS_PLATFORM CMD_SOFT_RESET
-
-	app_payload = compute_mac(command,seq_nr,telecommand_key) + seq_buf + command
-	#p "Payload:"
-	#p app_payload.split('').map(&:ord).map{|e| e.to_s(16)}
-
-	ax25_packet = callsign + (0x01).chr + callsign + [0x01,0x03,0xF0].map(&:chr).join + app_payload + "aaaa" # aaaa is a fake CRC
-	#p "AX25:"
-	#p ax25_packet.split('').map(&:ord).map{|e| e.to_s(16)}
-	p_size = ax25_packet.length
-	li_header = [LI_DIRECTION_OUTPUT,LI_CMD_RECEIVE_DATA,p_size >> 8, p_size & 0xFF].map(&:chr).join
-	chksum1 = fletcher_checksum16(li_header,li_header.length,0)
-	chksum1_str = [chksum1 >> 8, chksum1 & 0xFF].map(&:chr).join
-	
-	chksum2 = fletcher_checksum16(chksum1_str,2,chksum1)
-	chksum2 = fletcher_checksum16(ax25_packet,p_size,chksum2)
-	
-	chksum2_str = [chksum2 >> 8, chksum2 & 0xFF].map(&:chr).join
-	
-	li_packet = "He" + li_header + chksum1_str + ax25_packet + chksum2_str
-	
-	#p "Packet:"
-	#p li_packet.split('').map(&:ord).map{|e| e.to_s(16)}
-	
-	
-	
-	
-	
-	while(sem.synchronize{@manolito.closed?}) do
-		sleep 0.01
-	end
-	@manolito.send li_packet,0 rescue wait_for_manolito
-	#p 'Sent'
-
+	sem.synchronize {
+		@manolito.send(packet,0) rescue wait_for_manolito
+	}
 end
-#t2 = Thread.new do
-#	while(1) do
-#		if STDIN.ready?
-#			manolito.putc STDIN.getc
-#		end
-#	end
-#	sleep 10
-#end
-
 t.join
-#t2.join
